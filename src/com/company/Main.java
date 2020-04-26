412408e649757c79ba459159836808322170a74f
blekinis <blekinis89@gmail.com>
2020-04-26 12:26:42 +0200

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,21 @@ package com.company;
 
 public class Main {
 
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int a,b,c;
+        a=5;
+        b=10;
+        c= add(a,b);
+        System.out.println("Sum of two numbers is: " + c);
+
+
+
 	// write your code here
     }
 }
